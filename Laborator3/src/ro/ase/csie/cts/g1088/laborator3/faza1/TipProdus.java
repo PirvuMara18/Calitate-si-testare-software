112965ef9373e7cda6fef135951d2b74f456655c
mara.pirvu <mara.pirvu@0920IT0021.emag.local>
2021-03-11 12:36:05 +0200

    Update inainte de pauza

---
+++ Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza1/TipProdus.java
@@ -0,0 +1,19 @@
+package ro.ase.csie.cts.g1088.laborator3.faza1;
+
+public enum TipProdus {
+	NOU(0), DISCOUNT(0.1f), STOC_LIMITAT(0.5f), VECHI(0.35f);
+	
+	private final float discount;
+	
+	private TipProdus(float discount) {
+		this.discount = discount;
+	}
+	
+	//private TipProdus() {
+	//	this.discount = 0;
+	//}
+	
+	public float getDiscount() {
+		return this.discount;
+	}
+}
