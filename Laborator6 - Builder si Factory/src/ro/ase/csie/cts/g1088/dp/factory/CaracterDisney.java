382f6c9fef7ca69079c34d0629695baa7288dae6
mara.pirvu <mara.pirvu@0920IT0021.emag.local>
2021-04-01 13:29:01 +0300

    Implementare SimpleFactory

---
+++ Laborator6 - Builder si Factory/src/ro/ase/csie/cts/g1088/dp/factory/CaracterDisney.java
@@ -0,0 +1,24 @@
+package ro.ase.csie.cts.g1088.dp.factory;
+
+public class CaracterDisney extends CaracterAbstract {
+
+	String culoare;
+	boolean okPentruCopiiMici;
+
+	public CaracterDisney( String nume, String culoare, boolean ok) {
+		this.nume = nume; 
+		this.culoare = culoare;
+	}
+
+	@Override
+	public void alearga() {
+		System.out.println("Alearga...");
+		
+	}
+
+	@Override
+	public void sare() {
+		System.out.println("Sare...");
+		
+	};
+}
---
+++ Laborator6 - Builder si Factory/src/ro/ase/csie/cts/g1088/dp/factory/TestFactory.java
@@ -0,0 +1,22 @@
+package ro.ase.csie.cts.g1088.dp.factory;
+
+import java.util.ArrayList;
+
+public class TestFactory {
+
+	public static void main(String[] args) {
+		
+		
+		//CaracterDisney mickeyMouse = new CaracterDisney("rosu", "Mickey Mouse");
+		//CaracterMarvel spiderMan =new CaracterMarvel("Spiderman", 100);
+
+		
+//		CaracterAbstract mickeyMouse = new CaracterDisney("rosu", "Mickey Mouse");
+//		CaracterAbstract spiderMan =new CaracterMarvel("Spiderman", 100);
+		
+		CaracterAbstract mickeyMouse = FactoryCaractere.getCaracter(TipCaracter.DISNEY, "Mickey Mouse");
+//		
+		ArrayList<CaracterAbstract> caractere = new ArrayList<>();
+	}
+
+}
