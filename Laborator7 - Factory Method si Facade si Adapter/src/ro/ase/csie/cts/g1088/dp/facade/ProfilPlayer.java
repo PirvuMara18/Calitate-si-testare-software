144ce6254fdb0f6abc6648e9f0921adc971dcb03
mara.pirvu <mara.pirvu@0920IT0021.emag.local>
2021-04-08 13:30:50 +0300

    Implementare Facade si Adapter obiecte

---
+++ Laborator7 - Factory Method si Facade si Adapter/src/ro/ase/csie/cts/g1088/dp/facade/ProfilPlayer.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.g1088.dp.facade;
+
+public class ProfilPlayer {
+	
+	public String getProfil() {
+		return "Profil jucator";
+	}
+	
+
+}
--- Laborator7 - Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/FactoryModNormal.java
+++
@@ -1,5 +0,0 @@
-package ro.ase.csie.cts.g1088.dp.factory.method;
-
-public class FactoryModNormal {
-
-}
