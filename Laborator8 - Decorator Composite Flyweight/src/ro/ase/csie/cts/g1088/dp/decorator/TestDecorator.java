1339671d6f9790bce6d26d34c6def8bd35edba93
mara.pirvu <mara.pirvu@0920IT0021.emag.local>
2021-04-15 12:37:13 +0300

    Implementare Decorator

---
+++ Laborator8 - Decorator Composite Flyweight/src/ro/ase/csie/cts/g1088/dp/decorator/TestDecorator.java
@@ -0,0 +1,34 @@
+package ro.ase.csie.cts.g1088.dp.decorator;
+
+public class TestDecorator {
+
+	public static void main(String[] args) {
+
+		SuperErou dragon = new Dragon("Dragonul albastru", 1000);
+		dragon.alearga();
+		dragon.esteLovit(800);
+		dragon.seVindeca(100);
+		
+		SuperErou dragon2 = new Dragon("Dragonul galben", 1000);
+		dragon2.alearga();
+		dragon2.esteLovit(200);
+		dragon2.seVindeca(100);
+		
+		//aplicam decoratori pe primul dragon dar aici nu se fac modificari in mod dinamic
+//		SuperErou dragonCuArmura = new DecoratorArmura(dragon, 300);
+//		dragonCuArmura.esteLovit(400);
+//		
+		
+		
+		//este mai clean
+		dragon = new DecoratorArmura(dragon, 300);
+		dragon.esteLovit(400);
+		
+		dragon = new DecoratorEsteRanit(dragon);
+		dragon.esteLovit(400);
+		dragon.alearga();
+		
+
+	}
+
+}
